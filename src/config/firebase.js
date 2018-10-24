156b74b20b8938e0fb9c87015bba58929ced4681
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-10-24 12:11:51 +0100

    modify server to support plugin

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const config = {
   apiKey: process.env.APIKEY,
   authDomain: process.env.AUTHDOMAIN,
   databaseURL: process.env.DATABASEURL,
   projectId: process.env.PROJECTID,
   storageBucket: process.env.STORAGEBUCKET,
-  messagingSenderId: process.env.MESSAGINGSENDERID,
+  messagingSenderId: process.env.MESSAGINGDSENDERID,
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import Hapi from 'hapi';
 import firebase from 'firebase';
 import dotenv from 'dotenv';
 import { config } from './config/firebase';
+import Users from './controllers/Users';
 
 dotenv.config();
+
 firebase.initializeApp(config);
 
 const server = Hapi.server({
@@ -11,7 +13,26 @@ const server = Hapi.server({
   host: process.env.HOST,
 });
 
+server.route({
+  method: 'POST',
+  path: '/api/v1/users/signup',
+  handler: (request, reply) => Users.createUser(request.payload),
+});
+
 const init = async () => {
+  await server.register({
+    plugin: require('hapi-bodyparser'),
+    options: {
+      parser: { allowDots: true, strictNullHandling: true },
+      sanitizer: {
+        trim: true,
+        stripNullorEmpty: true,
+      },
+      merge: false,
+      body: false,
+    },
+  });
+
   await server.start();
   process.stdout.write(`server running on port ${process.env.PORT}\n`);
 };
