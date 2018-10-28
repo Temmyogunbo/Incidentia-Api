987e38bc056a3c732b994d364de55f76246a99d7
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-10-28 12:42:38 +0100

    add object validation for incidence

---
+++ src/schema/incidentiaSchema.js
@@ -0,0 +1,26 @@
+import Joi from 'joi';
+
+const incidentiaSchema = {
+  incidenciaName: Joi.string()
+    .min(3)
+    .required(),
+  reporter: Joi.string()
+    .email({ minDomainAtoms: 2 })
+    .required(),
+  severity: Joi.string()
+    .min(3)
+    .max(10)
+    .required(),
+  priority: Joi.string()
+    .min(3)
+    .max(10)
+    .required(),
+  cause: Joi.string()
+    .min(10)
+    .required(),
+  solution: Joi.string().min(10),
+  solved: Joi.boolean().required(),
+  date: Joi.date().timestamp().required()
+};
+
+export default incidentiaSchema;
