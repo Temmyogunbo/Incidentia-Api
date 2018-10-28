b34ce7b6c9898b3a9ba6ab0f3913c941df7fce83
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-10-28 12:41:04 +0100

    add glue manifest

---
+++ src/manifest.js
@@ -0,0 +1,48 @@
+import hapiBodyparser from 'hapi-bodyparser';
+import hapiPino from 'hapi-pino';
+import dotenv from 'dotenv';
+import { incidencia } from './routes/incidencia';
+
+dotenv.config();
+
+export const manifest = {
+  server: {
+    port: process.env.PORT,
+  },
+  register: {
+    plugins: [
+      {
+        plugin: hapiBodyparser,
+        options: {
+          parser: { allowDots: true, strictNullHandling: true },
+          sanitizer: {
+            trim: true,
+            stripNullorEmpty: true,
+          },
+          merge: false,
+          body: false,
+        },
+      },
+      {
+        plugin: hapiPino,
+        options: {
+          prettyPrint: true,
+          logEvents: ['response', 'onPostStart'],
+        },
+      },
+      {
+        plugin: './routes/incidencia',
+        routes: {
+          prefix: '/api/v1',
+        },
+      },
+    ],
+    options: {
+      once: true,
+    },
+  },
+};
+
+export const options = {
+  relativeTo: __dirname,
+};
