bafda95f2210a1b8641aee8e1f7d31f53d0fc2e8
temmyogunbo <Ogunbotemilola@yahoo.com>
2018-11-05 13:55:36 +0100

    setup db connection

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,19 @@
-import firebase from 'firebase';
-import { config } from '../config/firebase';
+import mongoose from 'mongoose';
 
-firebase.initializeApp(config[process.env.NODE_ENV]);
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect('mongodb://127.0.0.1:27017/incidencias');
+} else if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(
+    `mongodb+srv://temmyogunbo:${
+      process.env.MONGODB_ATLAS_PW
+    }@temmyogunb-vqhsp.mongodb.net/incidencias?retryWrites=true`,
+  );
+} else if (process.env.NODE_ENV === 'test') {
+  mongoose.connect('mongodb://127.0.0.1:27017/incidencias-test');
+}
+const db = mongoose.connection;
 
-export const db = firebase.database().ref('incidencias');
+db.on('error', () => process.stdout.write('A database error occured\n'));
+db.once('open', () => process.stdout.write('A database connection made\n'));
+
+export default db;
